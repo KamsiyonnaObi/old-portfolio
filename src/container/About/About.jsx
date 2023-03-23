9189a7e67973472942a7e3da4591cdfdfabb45fb
kamsi <obikamsi@gmail.com>
2023-03-23 12:04:06 -0500

    typos

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">I pair <span>Great Design</span><br /> with <span>real-world solutioons</span></h2>
+      <h2 className="head-text">I pair <span>Great Design</span><br /> with <span>real-world solutions</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
